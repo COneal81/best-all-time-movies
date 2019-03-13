2e2c5f0d8ea77a37f485e197dc46773d7f4dfc3f
Kari O'Neal <triadfirewood@gmail.com>
2019-03-13 18:57:56 +0000

    first commit

---
+++ lib/best/all/time/movies.rb
@@ -0,0 +1,12 @@
+require "best/all/time/movies/version"
+
+module Best
+  module All
+    module Time
+      module Movies
+        class Error < StandardError; end
+        # Your code goes here...
+      end
+    end
+  end
+end
---
+++ lib/best/all/time/movies/version.rb
@@ -0,0 +1,9 @@
+module Best
+  module All
+    module Time
+      module Movies
+        VERSION = "0.1.0"
+      end
+    end
+  end
+end
